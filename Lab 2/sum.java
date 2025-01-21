a49657820218c69055ca87efdd11ec06eb040ccc
Ambar-07 <ambargairola83@gmail.com>
2025-01-21 10:15:40 +0530

    new

---
+++ Lab 2/sum.java
@@ -0,0 +1,13 @@
+public class sum {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 10; i <= 950; i++) {
+            if (i % 6 == 0 && i % 9 == 0) {
+                sum += i;
+            }
+        }
+
+        System.out.print("The sum of all integers between 10 and 950 that are divisible by both 6 and 9 is: " + sum);
+    }
+}
